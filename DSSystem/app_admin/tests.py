83fe94c408bdcdb7ee19485d60e3b48c43928eb6
thinhtnp <thinhtnp2347@ut.edu.vn>
2025-01-15 23:15:16 +0700

    test.py/customer

--- DSSystem/app_admin/tests.py
+++ DSSystem/app_admin/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from django.utils.timezone import now
 from .models import User, Customer, LoyaltyCustomer, Promotion, Category, Product, Order, OrderDetail
 
-
 class UserModelTest(TestCase):
     def test_user_creation(self):
         user = User.objects.create_user(
@@ -16,4 +15,9 @@ class UserModelTest(TestCase):
         self.assertEqual(user.role, "customer")
         self.assertTrue(user.check_password("test123"))
 
+class CustomerModelTest(TestCase):
+    def test_customer_creation(self):
+        user = User.objects.create_user(username="testcustomer", password="test123")
+        customer = Customer.objects.create(user=user)
+        self.assertEqual(customer.user.username, "testcustomer")
 
