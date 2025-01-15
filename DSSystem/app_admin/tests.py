5fea18989efa273e7f7a30c1be0da63b9167de26
thinhtnp <thinhtnp2347@ut.edu.vn>
2025-01-15 23:36:55 +0700

    delete

--- DSSystem/app_admin/tests.py
+++ DSSystem/app_admin/tests.py
@@ -1,23 +1,3 @@
 from django.test import TestCase
-from django.utils.timezone import now
-from .models import User, Customer, LoyaltyCustomer, Promotion, Category, Product, Order, OrderDetail
 
-class UserModelTest(TestCase):
-    def test_user_creation(self):
-        user = User.objects.create_user(
-            username="testuser",
-            password="test123",
-            role="customer",
-            phone="123456789",
-            gender="male"
-        )
-        self.assertEqual(user.username, "testuser")
-        self.assertEqual(user.role, "customer")
-        self.assertTrue(user.check_password("test123"))
-
-class CustomerModelTest(TestCase):
-    def test_customer_creation(self):
-        user = User.objects.create_user(username="testcustomer", password="test123")
-        customer = Customer.objects.create(user=user)
-        self.assertEqual(customer.user.username, "testcustomer")
-
+# Create your tests here.
