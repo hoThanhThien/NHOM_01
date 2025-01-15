7cf8281cfe1fe0cbab601109eb3850be9bdf2c39
thinhtnp <thinhtnp2347@ut.edu.vn>
2025-01-15 22:55:07 +0700

    add test.py/users

--- DSSystem/app_admin/tests.py
+++ DSSystem/app_admin/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.utils.timezone import now
+from .models import User, Customer, LoyaltyCustomer, Promotion, Category, Product, Order, OrderDetail
 
-# Create your tests here.
+
+class UserModelTest(TestCase):
+    def test_user_creation(self):
+        user = User.objects.create_user(
+            username="testuser",
+            password="test123",
+            role="customer",
+            phone="123456789",
+            gender="male"
+        )
+        self.assertEqual(user.username, "testuser")
+        self.assertEqual(user.role, "customer")
+        self.assertTrue(user.check_password("test123"))
+
+
