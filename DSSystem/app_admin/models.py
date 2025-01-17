4b32adb2f8e65780e074487f5d3efa38372cd6f5
hoThanhThien <thienht2941@ut.edu.vn>
2025-01-18 02:18:57 +0700

    fix bug ( add, edit, delete user)          ( add, edit, delete product)

--- DSSystem/app_admin/models.py
+++ DSSystem/app_admin/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
+from django.contrib.admin.models import LogEntry
+
+
 
 # Gender Choices
 GENDER_CHOICES = (
